30083b6d283dfd7e440e573f6837c565c5396486
anthony cantor <ar.cantor@gmail.com>
2015-04-14 00:39:53 -0700

    added keccak_mac function

--- src/sha3.rs
+++ src/sha3.rs
@@ -43,6 +43,32 @@ pub fn new() -> Sha3 {
   Sha3::new()
 }
 
+pub fn digest(msg: &[u8]) -> [u8; 32] {
+  let mut sha3 = Sha3::new();
+  sha3.update(msg);
+  sha3.digest()
+}
+
+/// produce a keccak hmac; that is, an hmac done insecurely but its
+/// ok because keccak is crazy like that.
+/// http://en.wikipedia.org/wiki/Hash-based_message_authentication_code
+/// the hmac is: `keccak(secret || msg)`
+/// maybe dangerous?
+///
+/// # Panics
+/// - when secret.len() < 16
+/// - when msg is empty
+pub fn keccak_mac(secret: &[u8], msg: &[u8]) -> [u8; 32] {
+  assert!(secret.len() >= 16, "secret is dangerously small");
+  assert!(msg.len() > 0, "msg is empty");
+
+  let mut keccak = Sha3::new();
+
+  keccak.update(secret);
+  keccak.update(msg);
+  keccak.digest()
+}
+
 #[cfg(test)]
 mod test {
   extern crate rustc_serialize;
@@ -55,10 +81,38 @@ mod test {
     let expected = FromHex::from_hex(
       "4e03657aea45a94fc7d47ba826c8d667c0d1e6e33a64a036ec44f58fa12d6c45"
     ).unwrap();
+    let expected_bs = &expected[..];
 
     sha3.update(msg);
     let dgst = sha3.digest();
 
-    assert_eq!(&dgst[..], &expected[..]);
+    assert_eq!(&dgst[..], expected_bs);
+
+    assert_eq!(super::digest(msg), expected_bs);
+
+    let msg2 = b"uchk uchk chk uchk ucka chka chuk";
+    let expected2 = FromHex::from_hex(
+      "c51a4640694d14916a82ddd666d4ea63158745ed99e6cad1331f39c57e3abe37"
+    ).unwrap();
+    let expected2_bs = &expected2[..];
+
+    assert_eq!(super::digest(msg2), expected2_bs);    
+
+    assert_eq!(super::keccak_mac(&msg2[..16], &msg2[16..]),
+               expected2_bs);
+  }
+
+  mod keccak_mac {
+    #[test]
+    #[should_panic]
+    fn panic_secret() {
+      super::super::keccak_mac(b"too short", b"msg");
+    }
+
+    #[test]
+    #[should_panic]
+    fn panic_msg() {
+      super::super::keccak_mac(b"0123456789012345", b"");
+    }
   }
 }
