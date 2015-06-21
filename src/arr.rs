3dc19369e428ad1a0c0fb56953b150be7acdfedb
anthony cantor <cantora@gubbi.mobi>
2015-06-20 18:30:00 -0700

    arr trait impls

--- src/arr.rs
+++ src/arr.rs
@@ -1,8 +1,26 @@
 
+use std::cmp::{PartialEq, Eq, PartialOrd, Ord, Ordering};
+use std::fmt;
+use std::hash::{Hash, self};
+use std::iter::IntoIterator;
+use std::slice::{Iter, IterMut};
+
 macro_rules! array_impls {
   ($( $tname:ident , $N:expr );+) => {
     $(
       pub struct $tname<T> ([T; $N]);
+
+      impl<T> From<[T; $N]> for $tname<T> {
+        fn from(arr: [T; $N]) -> $tname<T> {
+          $tname(arr)
+        }
+      }
+
+      impl<T> Into<[T; $N]> for $tname<T> {
+        fn into(self) -> [T; $N] {
+          self.0
+        }
+      }
 
       impl<T> AsRef<[T]> for $tname<T> {
         #[inline]
@@ -48,41 +66,51 @@ macro_rules! array_impls {
         }
       }
 
+      impl<T: PartialEq> PartialEq for $tname<T> {
+        #[inline]
+        fn eq(&self, other: &Self) -> bool { &self.0[..] == &other.0[..] }
+        #[inline]
+        fn ne(&self, other: &Self) -> bool { &self.0[..] != &other.0[..] }
+      }
+
       impl<T: Eq> Eq for $tname<T> { }
 
       impl<T:PartialOrd> PartialOrd for $tname<T> {
         #[inline]
-        fn partial_cmp(&self, other: &[T; $N]) -> Option<Ordering> {
-          PartialOrd::partial_cmp(&&self[..], &&other[..])
+        fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+          PartialOrd::partial_cmp(&&self.0[..], &&other.0[..])
         }
         #[inline]
-        fn lt(&self, other: &[T; $N]) -> bool {
-          PartialOrd::lt(&&self[..], &&other[..])
+        fn lt(&self, other: &Self) -> bool {
+          PartialOrd::lt(&&self.0[..], &&other.0[..])
         }
         #[inline]
-        fn le(&self, other: &[T; $N]) -> bool {
-          PartialOrd::le(&&self[..], &&other[..])
+        fn le(&self, other: &Self) -> bool {
+          PartialOrd::le(&&self.0[..], &&other.0[..])
         }
         #[inline]
-        fn ge(&self, other: &[T; $N]) -> bool {
-          PartialOrd::ge(&&self[..], &&other[..])
+        fn ge(&self, other: &Self) -> bool {
+          PartialOrd::ge(&&self.0[..], &&other.0[..])
         }
         #[inline]
-        fn gt(&self, other: &[T; $N]) -> bool {
-          PartialOrd::gt(&&self[..], &&other[..])
+        fn gt(&self, other: &Self) -> bool {
+          PartialOrd::gt(&&self.0[..], &&other.0[..])
         }
       }
 
       impl<T:Ord> Ord for $tname<T> {
         #[inline]
         fn cmp(&self, other: &$tname<T>) -> Ordering {
-          Ord::cmp(&&self[..], &&other[..])
+          Ord::cmp(&&self.0[..], &&other.0[..])
         }
       }
     )+
   }
 }
 
-//array_impls! {
-//  Arr20,20
-//}
+array_impls! {
+  Arr28, 28;
+  Arr32, 32;
+  Arr48, 48;
+  Arr64, 64
+}
